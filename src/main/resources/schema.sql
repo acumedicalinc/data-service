3cf73a662ba59d7b0ebb2971475f5408d1a99dac
Julia <jlchen6@wisc.edu>
2017-02-21 16:17:24 -0600

    changed schema with mysql syntax
    also changed application.properties to reduce logging which was slowing
    down uploading.

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,11 +1,31 @@
 drop table if exists users;
-create table users(id bigint auto_increment, username varchar(255), password varchar(255), enabled boolean);
-insert into users(username,password,enabled) values('frank','cGFzc3dvcmQ=',true);
-insert into users(username,password,enabled) values('julia','cGFzc3dvcmQ=',true);
-insert into users(username,password,enabled) values('user','password',true);
+create table users(
+	`id` bigint auto_increment,
+	`username` varchar(255),
+	`password` varchar(255),
+	`enabled` boolean,
+	PRIMARY KEY(`id`));
+    
+insert into users(`username`,`password`,`enabled`)
+values('frank','cGFzc3dvcmQ=',true);
+
+insert into users(`username`,`password`,`enabled`)
+values('julia','cGFzc3dvcmQ=',true);
+
+insert into users(`username`,`password`,`enabled`)
+values('user','password',true);
 
 drop table if exists authorities;
-create table authorities(id bigint auto_increment, username  varchar(255),authority  varchar(255), UNIQUE(username,authority));
-insert into authorities(username,authority) values('julia','admin');
-insert into authorities(username,authority) values('frank','superadmin');
-insert into authorities(username,authority) values('user','USER');
+create table authorities(
+	`id` bigint auto_increment, 
+	`username`  varchar(255),
+	`authority`  varchar(255), 
+	UNIQUE(`username`,`authority`),
+	PRIMARY KEY(`id`));
+    
+insert into authorities(`username`,`authority`)
+values('julia','admin');
+insert into authorities(`username`,`authority`)
+values('frank','superadmin');
+insert into authorities(`username`,`authority`)
+values('user','USER');
